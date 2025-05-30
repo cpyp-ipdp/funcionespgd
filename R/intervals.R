732e84a0c56a2cabcaf9cb4056520a46b4c92db1
IPDP CDMX <cpyp.ipdp@gmail.com>
2025-05-30 11:20:18 -0600

    Update intervals.R

--- R/intervals.R
+++ R/intervals.R
@@ -5,7 +5,7 @@
 #' @param margen Margen proporcional (por ejemplo 0.05 para ±5%)
 #' @return El mismo data.frame con columnas adicionales: 'superior' e 'inferior'
 #' @export
-añade_intervalos <- function(data, var = "porcentaje", margen = 0.05) {
+intervals <- function(data, var = "porcentaje", margen = 0.05) {
   data$superior <- data[[var]] * (1 + margen)
   data$inferior <- data[[var]] * (1 - margen)
   return(data)
