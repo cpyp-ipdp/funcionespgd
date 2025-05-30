6333afd9ddf72007cfb0b62d145c7361402b048d
IPDP CDMX <cpyp.ipdp@gmail.com>
2025-05-30 12:36:32 -0600

    Update intervals.R

--- R/intervals.R
+++ R/intervals.R
@@ -1,29 +1,22 @@
-#' Añade columnas estimado/superior/inferior, con o sin intervalos
+#' Intervalos que se expanden año con año desde un punto base
 #'
-#' @param data Data frame con columna `year` y una variable numérica
-#' @param var Nombre de la columna con los valores base
-#' @param margen Margen porcentual para los intervalos (ej. 0.01 = ±1%)
-#' @param aplicar_intervalos Lógico. Si FALSE, copia el mismo valor a todas las columnas.
+#' @param data Data frame con year y variable base
+#' @param var Variable base para el valor estimado
+#' @param margen Margen de crecimiento anual (ej. 0.01 = ±1% acumulativo por año)
+#' @param año_base Año a partir del cual se empiezan a expandir los intervalos
 #' @return Data frame con columnas: year, estimado, superior, inferior
 #' @export
-intervals <- function(data, var = "porcentaje", margen = 0.05, aplicar_intervalos = TRUE) {
-  valor <- data[[var]]
-  
-  if (aplicar_intervalos) {
-    estimado  <- valor
-    superior  <- valor * (1 + margen)
-    inferior  <- valor * (1 - margen)
-  } else {
-    estimado  <- valor
-    superior  <- valor
-    inferior  <- valor
-  }
+intervals <- function(data, var = "porcentaje", margen = 0.01, año_base = 2022) {
+  data <- data %>%
+    dplyr::mutate(
+      year = as.numeric(year),
+      estimado = .data[[var]],
+      diferencia = pmax(0, year - año_base),  # 0 si es año base o anterior
+      factor = (1 + margen) ^ diferencia,
+      superior = estimado * factor,
+      inferior = estimado / factor
+    ) %>%
+    dplyr::select(year, estimado, superior, inferior)
 
-  return(dplyr::tibble(
-    year = as.numeric(data$year),
-    estimado = estimado,
-    superior = superior,
-    inferior = inferior
-  ))
+  return(data)
 }
-
